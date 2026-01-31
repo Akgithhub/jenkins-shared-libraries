2a553a63b09354abac3600abc4cfb1b3356c2158
Amaan Khan <147619523+Akgithhub@users.noreply.github.com>
2026-01-31 23:35:13 +0530

    Fix string interpolation in git command

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(String url, String branch){
   echo 'This is a Code stage'
-  git url: ${url}, branch: ${branch}
+  git url: "${url}", branch: "${branch}"
   echo 'Repo is cloned sucessfully'
 }
